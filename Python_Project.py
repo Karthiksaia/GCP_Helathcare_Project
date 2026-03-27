d8d45dc968fa372ffe09e6856a3d35b57b18503e
Karthik Achalla <saik74847@gmail.com>
2026-03-27 17:09:07 -0400

    adding the first commit

---
+++ Python_Project.py
@@ -0,0 +1,8 @@
+winners = ["Alice", "Bob", "Charlie", "Charlie", "Bob"]
+scoreboard_counts = {}
+for winner in winners:
+    if winner in scoreboard_counts:
+        scoreboard_counts[winner] += 1
+    else:
+        scoreboard_counts[winner] = 1
+print(scoreboard_counts)
